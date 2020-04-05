1a75f597ba5883b4a99b4bc7ea12c66c4655d24e
unknown <tim.janzelj@gmail.com>
2020-04-05 14:17:46 +0200

    Fade animations for score panel

--- components/score-panel/score-panel.component.jsx
+++ components/score-panel/score-panel.component.jsx
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View } from 'react-native';
+import { Animated } from 'react-native';
 import { ScorePanelContainer, Score, BestScore } from './score-panel.styles';
 
-const ScorePanel = ({ score, bestScore, isNewBestScore }) => {
-	return (
-		<ScorePanelContainer>
-			{
-				score ?
-				<View>
+class ScorePanel extends Component {
+
+	state = {
+		fadeAnimation: new Animated.Value(0),
+		score: null
+	};
+
+	static getDerivedStateFromProps(nextProps) {
+		if (nextProps.score > 0) {
+			return {
+				score: nextProps.score
+			}
+		}
+
+		return {};
+	}
+	
+	componentDidUpdate(prevProps) {
+		if (this.props.score && !prevProps.score) {
+			this.fadeIn();
+		}
+		if (!this.props.score && prevProps.score) {
+			this.fadeOut();
+		}
+	}
+
+  fadeIn = () => {
+    Animated.timing(this.state.fadeAnimation, {
+      toValue: 1,
+			duration: 2000,
+			useNativeDriver: true
+    }).start();
+  };
+
+  fadeOut = () => {
+    Animated.timing(this.state.fadeAnimation, {
+      toValue: 0,
+			duration: 200,
+			useNativeDriver: true
+    }).start(() => {
+			this.setState({
+				score: this.props.score
+			});
+		});
+  };
+
+	render() {
+		const { bestScore, isNewBestScore } = this.props;
+		const { score } = this.state;
+		return (
+			<ScorePanelContainer>
+				<Animated.View
+					style={[{
+						opacity: this.state.fadeAnimation
+					}]}
+				>
 					<Score>{score}</Score>
 					{
 						bestScore ?
@@ -19,12 +69,10 @@ const ScorePanel = ({ score, bestScore, isNewBestScore }) => {
 						:
 						null
 					}
-				</View>
-				:
-				null
-			}
-		</ScorePanelContainer>
-	);
+				</Animated.View>
+			</ScorePanelContainer>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
